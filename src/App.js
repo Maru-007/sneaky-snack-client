32b8604c6ef2a4c021a15358d1f7cd714a8de424
stacyyuu <stacyyuu@gmail.com>
2023-05-09 02:35:47 +0000

    client

---
+++ src/App.js
@@ -0,0 +1,127 @@
+import React, { useEffect, useState } from "react";
+import { socket } from "./socket";
+import { EVENT_NAMES } from "./utils";
+import "./App.scss";
+import bannerImage from "./Banner.png";
+import Room from "./Room";
+
+const rooms = [
+  "kidsroom",
+  "bathroom",
+  "parentsroom",
+  "hallway",
+  "kitchen",
+  "livingroom",
+  "garage",
+];
+
+const App = () => {
+  const [isConnected, setIsConnected] = useState(false);
+  const [question, setQuestion] = useState("");
+  const [choices, setChoices] = useState([]);
+  const [message, setMessage] = useState("");
+  const [viewBanner, setViewBanner] = useState(true);
+  const [currentRoom, setCurrentRoom] = useState("kidsroom");
+  const [displayRoom, setDisplayRoom] = useState(false);
+
+  useEffect(() => {
+    function handleConnect() {
+      setIsConnected(true);
+      console.log("handleConnect has been triggered");
+      setQuestion("");
+      setChoices([]);
+    }
+
+    function handleDisconnect() {
+      setIsConnected(false);
+      console.log("handleDisconnect has been triggered");
+      setQuestion("");
+      setChoices([]);
+    }
+
+    // these are our listeners
+    socket.on("connect", handleConnect);
+    socket.on("disconnect", handleDisconnect);
+    socket.on("response", (payload) => console.log(payload));
+    socket.on(EVENT_NAMES.questionsReady, (question) => {
+      setQuestion(question);
+      setChoices(question.choices);
+      console.log(question);
+      console.log(question.choices);
+    });
+
+    socket.on(EVENT_NAMES.message, (message) => {
+      console.log(message);
+      setMessage(message);
+    });
+    // clean up the socket listeners
+    return () => {
+      // turns off socket listeners
+      socket.off("connect", handleConnect);
+      socket.off("disconnect", handleDisconnect);
+      socket.off("response", () => console.log("response listener is off"));
+    };
+  }, []);
+
+  const handleReady = () => {
+    socket.emit(EVENT_NAMES.childReady);
+  };
+
+  const handleChoice = (choice) => {
+    setMessage("");
+    console.log(choice);
+    socket.emit(EVENT_NAMES.selection, choice);
+    setViewBanner(false);
+    setDisplayRoom(true);
+
+    if (rooms.includes(choice)) setCurrentRoom(choice);
+    console.log(currentRoom);
+  };
+
+  return (
+    <div>
+      <p>{isConnected ? "connected" : "not connected"}</p>
+      <br></br>
+      {viewBanner && (
+        <>
+          <img src={bannerImage} alt="banner" />
+          <br></br>
+          <button className="startButton" onClick={handleReady}>
+            Start new game
+          </button>
+        </>
+      )}
+
+      <div>
+
+        {displayRoom && (
+          <Room
+            currentRoom={currentRoom}
+            handleChoice={handleChoice}
+            rooms={rooms}
+          />
+        )}
+
+        {message}
+        <br></br>
+        <br></br>
+        {question.message}
+
+        <div className="choices">
+          {choices &&
+            choices.map((choice) => (
+              <button
+                key={choice}
+                value={choice}
+                onClick={(e) => handleChoice(choice)}
+              >
+                {choice}
+              </button>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Room.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+const Room = ({ currentRoom, handleDoorClick, choices, handleChoice }) => {
+  const backgroundUrl = `url(/${currentRoom}.png)`;
+
+  return (
+    <div
+      className="room"
+      style={{
+        position: "relative",
+        height: "600px",
+        backgroundImage: backgroundUrl,
+        backgroundSize: "contain",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center center",
+      }}
+    >
+
+      {/* {choices &&
+        choices.map((choice) => (
+          <button
+            key={choice}
+            value={choice}
+            onClick={(e) => handleChoice(choice)}
+            className="door"
+            style={{
+              position: "absolute",
+              top: "30%",
+              left: "40vw",
+              width: "60px",
+              height: "100px",
+              backgroundColor: "brown",
+              borderRadius: "5%",
+            }}
+          >
+            <p style={{ fontSize: "12px", color: "white" }}>{choice}</p>
+          </button>
+        ))} */}
+    </div>
+  );
+};
+
+export default Room;
