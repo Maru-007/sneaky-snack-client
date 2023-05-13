a339864952f0cc0b9402528fd3391fb7f3d73a58
stacyyuu <stacyyuu@gmail.com>
2023-05-13 23:18:01 +0000

    morestyle & volume slider

--- src/Sound/index.js
+++ src/Sound/index.js
@@ -3,16 +3,18 @@ import Sound from "react-sound";
 import background from "./audioFiles/background1.mp3";
 import door from "./audioFiles/door.mp3"
 
-const PlaySound = ({isPlaying, doorSound}) => {
+const PlaySound = ({isPlaying, doorSound, volume}) => {
   return (
     <div>
       <Sound
         url={background}
         playStatus={isPlaying ? Sound.status.PLAYING : Sound.status.STOPPED}
+        volume={volume}
         loop={true}
       />
       <Sound
         url={door}
+        volume={volume}
         playStatus={doorSound ? Sound.status.PLAYING : Sound.status.STOPPED}
       />
     </div>
