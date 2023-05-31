7f9ff6ea643fc1f2c1da9dfa87017d218722c8e9
Ezgi <107739024+ezgi-c@users.noreply.github.com>
2023-05-30 22:06:50 -0700

    sounds converted to howler

--- src/Sound/index.js
+++ src/Sound/index.js
@@ -1,6 +1,5 @@
-
-import { Howl, Howler } from 'howler';
-import { useEffect } from 'react';
+import { Howl, Howler } from "howler";
+import { useEffect } from "react";
 
 import background from "./audioFiles/background1.mp3";
 import door from "./audioFiles/door.mp3";
@@ -18,9 +17,21 @@ import flashlight from "./audioFiles/flashlight.wav";
 import tennisball from "./audioFiles/tennisball.wav";
 import win from "./audioFiles/win.mp3";
 import childwin from "./audioFiles/childwin.wav";
+import dogwin from "./audioFiles/dogwin.wav";
 import lose from "./audioFiles/lose.wav";
 import negative from "./audioFiles/negative.mp3";
-import dogwin from "./audioFiles/dogwin.wav";
+import stereo from "./audioFiles/stereo.wav";
+import slam from "./audioFiles/slam.wav";
+import cologne from "./audioFiles/cologne.wav";
+import hairdryer from "./audioFiles/hairdryer.wav";
+import alarm from "./audioFiles/alarm.wav";
+import thermostat from "./audioFiles/thermostat.wav";
+import pillow from "./audioFiles/pillow.wav";
+import bowl from "./audioFiles/bowl.wav";
+import bark from "./audioFiles/bark.wav";
+import roomba from "./audioFiles/roomba.wav";
+import mouse from "./audioFiles/mouse.wav";
+import flowerpot from "./audioFiles/flowerpot.wav";
 
 const PlaySound = ({
   isPlaying,
@@ -31,28 +42,47 @@ const PlaySound = ({
   winState,
   loseState,
   distract,
-  dogDistract
+  dogDistract,
 }) => {
-  useEffect(()=> {
-    
+  useEffect(() => {
     const sounds = {
       background: new Howl({ src: [background], volume: volume, loop: true }),
-      door: new Howl({ src: [door], volume: .1 }),
-      backpack: new Howl({ src: [backpack], volume: .5, pos: 17 }),
-      stepstool: new Howl({ src: [stepstool], volume: .5 }),
-      slipper: new Howl({ src: [slipper], volume: .5 }),
-      umbrella: new Howl({ src: [umbrella], volume: .5 }),
-      baseballmitt: new Howl({ src: [baseballmitt], volume: .5 }),
-      remote: new Howl({ src: [remote], volume: .5 }),
-      sock: new Howl({ src: [sock], volume: .35 }),
-      toiletpaper: new Howl({ src: [toiletpaper], volume: .5 }),
-      squeakytoy: new Howl({ src: [squeakytoy], volume: .5 }),
-      shoe: new Howl({ src: [shoe], volume: .5 }),
-      flashlight: new Howl({ src: [flashlight], volume: .5 }),
-      tennisball: new Howl({ src: [tennisball], volume: .5 }),
+      door: new Howl({ src: [door], volume: 0.1 }),
+      backpack: new Howl({ src: [backpack], volume: 0.5, pos: 17 }),
+      stepstool: new Howl({ src: [stepstool], volume: 0.5 }),
+      slipper: new Howl({ src: [slipper], volume: 0.5 }),
+      umbrella: new Howl({ src: [umbrella], volume: 0.5 }),
+      baseballmitt: new Howl({ src: [baseballmitt], volume: 0.5 }),
+      remote: new Howl({ src: [remote], volume: 0.5 }),
+      sock: new Howl({ src: [sock], volume: 0.35 }),
+      toiletpaper: new Howl({ src: [toiletpaper], volume: 0.5 }),
+      squeakytoy: new Howl({ src: [squeakytoy], volume: 0.5 }),
+      shoe: new Howl({ src: [shoe], volume: 0.5 }),
+      flashlight: new Howl({ src: [flashlight], volume: 0.5 }),
+      tennisball: new Howl({ src: [tennisball], volume: 0.5 }),
+      win: new Howl({ src: [win], volume: 0.5 }),
+      childwin: new Howl({ src: [childwin], volume: 0.1 }),
+      dogwin: new Howl({ src: [dogwin], volume: 0.7 }),
+      lose: new Howl({ src: [lose], volume: 0.3 }),
+      negative: new Howl({ src: [negative], volume: 0.5 }),
+      stereo: new Howl({ src: [stereo], volume: 0.35 }),
+      slam: new Howl({ src: [slam], volume: 0.35 }),
+      cologne: new Howl({ src: [cologne], volume: 0.5 }),
+      hairdryer: new Howl({ src: [hairdryer], volume: 0.9 }),
+      alarm: new Howl({ src: [alarm], volume: 0.5 }),
+      thermostat: new Howl({ src: [thermostat], volume: 0.5 }),
+      pillow: new Howl({ src: [pillow], volume: 0.5 }),
+      bowl: new Howl({ src: [bowl], volume: 0.5 }),
+      bark: new Howl({ src: [bark], volume: 0.5 }),
+      roomba: new Howl({ src: [roomba], volume: 0.5 }),
+      mouse: new Howl({ src: [mouse], volume: 0.5 }),
+      flowerpot: new Howl({ src: [flowerpot], volume: 0.5 }),
     };
-    isPlaying ? sounds.background.play() : sounds.background.stop();
+
+    if (isPlaying === true) sounds.background.play()
+    // isPlaying ? sounds.background.play() : sounds.background.stop();
     doorSound ? sounds.door.play() : sounds.door.stop();
+
     if (item === "backpack") sounds.backpack.play();
     if (item === "stepstool") sounds.stepstool.play();
     if (item === "slipper") sounds.slipper.play();
@@ -66,25 +96,45 @@ const PlaySound = ({
     if (dogItem === "flashlight") sounds.flashlight.play();
     if (dogItem === "tennis ball") sounds.tennisball.play();
 
+    if (distract === "stereo") sounds.stereo.play();
+    if (distract === "slam") sounds.slam.play();
+    if (distract === "cologne") sounds.cologne.play();
+    if (distract === "hairdryer") sounds.hairdryer.play();
+    if (distract === "alarm") sounds.alarm.play();
+    if (distract === "thermostat") sounds.thermostat.play();
+    if (dogDistract === "pillow") sounds.pillow.play();
+    if (dogDistract === "bowl") sounds.bowl.play();
+    if (dogDistract === "murderers") sounds.bark.play();
+    if (dogDistract === "roomba") sounds.roomba.play();
+    if (dogDistract === "mouse") sounds.mouse.play();
+    if (dogDistract === "flower pot") sounds.flowerpot.play();
 
+    if (winState === true) {
+      sounds.win.play();
+      sounds.childwin.play();
+      sounds.dogwin.play();
+    }
 
-
-
-
-
-
-    
-
-
+    if (loseState === true) {
+      sounds.lose.play();
+      sounds.negative.play();
+    }
 
     return () => {
       Howler.stop();
-    }
-  },[volume,isPlaying,doorSound, item, dogItem])
-  return (
-    <div></div>
-   
-  );
+    };
+  }, [
+    volume,
+    isPlaying,
+    doorSound,
+    item,
+    dogItem,
+    winState,
+    loseState,
+    distract,
+    dogDistract
+  ]);
+  return <div></div>;
 };
 
 export default PlaySound;
